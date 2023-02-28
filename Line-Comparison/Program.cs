429eb5b2fff5d6385106d46a35c76cfec3d806f1
Bhagya Raju <bhagyarajusrivatsa12@gmail.com>
2023-02-28 23:57:51 +0530

    Geomentry Length in Line Comparison

--- Line-Comparison/Program.cs
+++ Line-Comparison/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line-Comparison");
+            //Console.WriteLine("Welcome to Line-Comparison");
+            Line_Comparison.Geomentry_Length();
         }
     }
 }
